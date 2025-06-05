81c86ef9b0a07ccb850156e2a254346e012b2434
Tereza Krystofova <tereza.krystofova@gmail.com>
2025-06-05 19:40:40 +0200

    2025_06_05

--- exercise/cypress-tests/cypress.config.ts
+++ exercise/cypress-tests/cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   watchForFileChanges: false,
   defaultCommandTimeout: 15000,
+  projectId: "5x84gp",
   e2e: {
     baseUrl: 'https://qa-classic.myshoptet.com',
     setupNodeEvents(on, config) {
